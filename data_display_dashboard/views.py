be6a9d80d2bb70f5692bfbb4b5f844703a4e2f37
Cole Sturza <colesturza@gmail.com>
2022-01-15 13:26:30 -0700

    Lots of changes.

--- data_display_dashboard/views.py
+++ data_display_dashboard/views.py
@@ -19,18 +19,14 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name=None):
-    return render_template("hello_there.html", name=name, date=datetime.now())
-
-
 @app.route("/api/data")
 def get_data():
 
     data = {
-        "x": int(round(datetime.now().timestamp() * 1000)),
-        "y": [round(random.random(), 2) for _ in range(2)]
+        "x":
+        int(round(datetime.now().timestamp() * 1000)),
+        "y": [(1.0 if random.random() > 0.5 else -1.0) *
+              round(random.random() * 100) for _ in range(2)]
     }
 
     return jsonify(data)
